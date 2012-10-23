83a288097ec2445b24496fb46d05c22b8e4bc6d2
Vasileios Georgitzikis <tzikis@gmail.com>
2012-10-24 00:08:24 +0300

    updated to work with base64-encoded bin files

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
+
 $directory = "tempfiles/";
-if(!isset($_REQUEST['hex']))
-	die(json_encode(array('success' => 0, 'text' => "NO HEX!")));
+if(!isset($_REQUEST['bin']))
+	die(json_encode(array('success' => 0, 'text' => "NO BIN!")));
 
 if(!isset($_REQUEST['ip']))
 	die(json_encode(array('success' => 0, 'text' => "NO IP!")));
-	
-$value = $_REQUEST['hex'];
+
+$value = $_REQUEST['bin'];
 $ip = $_REQUEST['ip'];
 
 // echo($value);
@@ -16,11 +17,12 @@ do
 {
 	$filename = genRandomString(10);
 }
-while(file_exists($filename.".hex"));
-$file = fopen($directory.$filename.".hex", 'x');
+while(file_exists($filename.".bin"));
+$file = fopen($directory.$filename.".bin", 'x');
+
 if($file)
 {
-	fwrite($file, $value);
+	fwrite($file, base64_decode($value));
 	fclose($file);
 }
 
@@ -29,16 +31,17 @@ if(isset($_REQUEST['pass']))
 	//
 }
 
-$output = dothat($filename, "avr-objcopy  -I ihex ".$directory.$filename.".hex -O binary ".$directory.$filename.".bin 2>&1");
-if($output["error"])
-{
-	$output["success"] = 0;
-	$output["text"] = "Uknown Upload Error!";
-	$output["lines"] = array(0);
-	cleanDir($directory.$filename);
-	die(json_encode($output));
-}
-	
+//$output = dothat($filename, "avr-objcopy  -I ihex ".$directory.$filename.".hex -O binary ".$directory.$filename.".bin 2>&1");
+
+//if($output["error"])
+//{
+//	$output["success"] = 0;
+//	$output["text"] = "Uknown Upload Error!";
+//	$output["lines"] = array(0);
+//	cleanDir($directory.$filename);
+//	die(json_encode($output));
+//}
+
 // $curDir = getcwd();
 // chdir($directory);
 $output = dothat($filename, "cd $directory ; tftp -R 4000:5000 -v -m octet $ip -c put ".$filename.".bin 2>&1"); // *.o -> *.elf
@@ -55,7 +58,7 @@ if($output["error"])
 			$output["text"] = "Transfer timed out.";
 			$output["lines"] = array(0);
 			cleanDir($directory.$filename);
-			die(json_encode($output));			
+			die(json_encode($output));
 		}
 	}
 }
@@ -63,13 +66,13 @@ if($output["error"])
 
 cleanDir($directory.$filename);
 // echo(json_encode($output));
-echo(json_encode(array('success' => 1, 'text' => "Uploaded successfully! ".$output["output"][count($output["output"])-1] )));	
+echo(json_encode(array('success' => 1, 'text' => "Uploaded successfully! ".$output["output"][count($output["output"])-1] )));
 
 function genRandomString($length)
 {
     // $length = 10;
     $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $string = "";    
+    $string = "";
     for ($p = 0; $p < $length; $p++)
 	{
         $string .= $characters{mt_rand(0, strlen($characters)-1)};
@@ -79,16 +82,16 @@ function genRandomString($length)
 
 function cleanDir($filename)
 {
-	
-	// if(file_exists($filename.".hex")) unlink($filename.".hex");	
-	// if(file_exists($filename.".bin")) unlink($filename.".bin");	
+
+	// if(file_exists($filename.".hex")) unlink($filename.".hex");
+	// if(file_exists($filename.".bin")) unlink($filename.".bin");
 	// Remeber to suggest a cronjob, in case something goes wrong...
 	// find $path -name $filename.{o,cpp,elf,hex} -mtime +1 -delete
 }
 
 function dothat($filename, $cmd)
 {
-	exec($cmd, $out, $ret); 
+	exec($cmd, $out, $ret);
 	$return_val = false;
 	if($ret)
 	{
